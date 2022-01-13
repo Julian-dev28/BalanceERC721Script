0ff931191db298fd8050d33648f496b274fafc0f
Julian-dev28 <julian_martinez28@outlook.com>
2022-01-12 23:19:22 -0800

    Create BalanceCheckerAVAX.sol

---
+++ contracts/BalanceCheckerAVAX.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+struct OwnerAndBalance {
+  address owner;
+  uint256 balance;
+}
+
+contract BalanceCheckerAVAX {
+  function tokenBalance(address[] memory userAddresses)
+    public
+    view
+    returns (OwnerAndBalance[] memory holders)
+  {
+    OwnerAndBalance[] memory owners = new OwnerAndBalance[](userAddresses.length);
+
+    for (uint256 i = 0; i < userAddresses.length; i++) {
+      owners[i].owner = address(userAddresses[i]);
+      owners[i].balance = address(userAddresses[i]).balance;
+    }
+
+    holders = owners;
+  }
+}
