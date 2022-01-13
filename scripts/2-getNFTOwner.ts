d079aa06e52252b7bb9dcfb9d1e777a5e2b9dbac
Julian-dev28 <julian_martinez28@outlook.com>
2022-01-13 09:09:12 -0800

    Update 2-getNFTOwner.ts

--- scripts/2-getNFTOwner.ts
+++ scripts/2-getNFTOwner.ts
@@ -1,14 +1,14 @@
 const { ethers, waffle} = require("hardhat");
-import {BULKTOKENIDS} from './constants';
+import {BULKTOKENIDS, tokenAbi} from './constants';
 import {nftAbi} from './constants'
 
 async function main() {
+    const provider = ethers.provider
     const tokenId = 50
     const NFTAddress = '0x0c2485d08c7611ceafdbb370dad4fa86b5935ff4';
-    const contract = await ethers.getContractFactory('ERC721')
-    const NFT = await contract.attach(NFTAddress)
-    const NFTOWner = await NFT.ownerOf(tokenId)
-    console.log({tokenId, NFTOWner})
+    let nft = new ethers.Contract(NFTAddress, nftAbi, provider)
+    let owner = await nft.ownerOf(tokenId) 
+    console.log({tokenId, owner})
 }
 
 main()
