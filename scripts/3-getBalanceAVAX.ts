faeb636e2efe4cccd98b83c281da01e2a306100f
Julian-dev28 <julian_martinez28@outlook.com>
2022-01-12 23:36:17 -0800

    script check Avax update

--- scripts/3-getBalanceAVAX.ts
+++ scripts/3-getBalanceAVAX.ts
@@ -3,9 +3,8 @@ import { ADDRESSES } from './constants';
 
 async function main() {
     
-    const address = '0xD3F4C46A7CB7b74c5183F950C8e7aFBA90466148'
     const contract = await ethers.getContractFactory('BalanceCheckerAVAX')
-    const BalanceChecker = await contract.attach(address)
+    const BalanceChecker = await contract.attach('0xD3F4C46A7CB7b74c5183F950C8e7aFBA90466148')
 
     const balances = await BalanceChecker.tokenBalance(ADDRESSES)
     console.log({balances})
